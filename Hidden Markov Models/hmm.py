f30c8e37aa2952a5427427d71eca8d0012b114b0
Vivek Bharadwaj <vivekdib@usc.edu>
2019-11-04 00:12:39 -0800

    PA4 starter code
    Programming Assignment 4 Starter Code
    Part 1: K-Means Clustering
    Part 2: Hidden Markov Models

---
+++ Hidden Markov Models/hmm.py
@@ -0,0 +1,117 @@
+from __future__ import print_function
+import numpy as np
+
+
+class HMM:
+
+    def __init__(self, pi, A, B, obs_dict, state_dict):
+        """
+        - pi: (1*num_state) A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+        - A: (num_state*num_state) A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+        - B: (num_state*num_obs_symbol) A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+        - obs_dict: (num_obs_symbol*1) A dictionary mapping each observation symbol to their index in B
+        - state_dict: (num_state*1) A dictionary mapping each state to their index in pi and A
+        """
+        self.pi = pi
+        self.A = A
+        self.B = B
+        self.obs_dict = obs_dict
+        self.state_dict = state_dict
+
+    def forward(self, Osequence):
+        """
+        Inputs:
+        - self.pi: (1*num_state) A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+        - self.A: (num_state*num_state) A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+        - self.B: (num_state*num_obs_symbol) A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - alpha: (num_state*L) A numpy array alpha[i, t] = P(Z_t = s_i, x_1:x_t | λ)
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        alpha = np.zeros([S, L])
+        ###################################################
+        # Edit here
+        ###################################################
+        return alpha
+
+    def backward(self, Osequence):
+        """
+        Inputs:
+        - self.pi: (1*num_state) A numpy array of initial probailities. pi[i] = P(Z_1 = s_i)
+        - self.A: (num_state*num_state) A numpy array of transition probailities. A[i, j] = P(Z_t = s_j|Z_t-1 = s_i)
+        - self.B: (num_state*num_obs_symbol) A numpy array of observation probabilities. B[i, k] = P(X_t = o_k| Z_t = s_i)
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - beta: (num_state*L) A numpy array beta[i, t] = P(x_t+1:x_T | Z_t = s_i, λ)
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        beta = np.zeros([S, L])
+        ###################################################
+        # Edit here
+        ###################################################
+        return beta
+
+    def sequence_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - prob: A float number of P(x_1:x_T | λ)
+        """
+        prob = 0
+        ###################################################
+        # Edit here
+        ###################################################
+        return prob
+
+    def posterior_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - prob: (num_state*L) A numpy array of P(s_t = i|O, λ)
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        prob = np.zeros([S, L])
+        ###################################################
+        # Edit here
+        ###################################################
+        return prob
+    #TODO:
+    def likelihood_prob(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - prob: (num_state*num_state*(L-1)) A numpy array of P(X_t = i, X_t+1 = j | O, λ)
+        """
+        S = len(self.pi)
+        L = len(Osequence)
+        prob = np.zeros([S, S, L - 1])
+        ###################################################
+        # Edit here
+        ###################################################
+        return prob
+
+    def viterbi(self, Osequence):
+        """
+        Inputs:
+        - Osequence: (1*L) A numpy array of observation sequence with length L
+
+        Returns:
+        - path: A List of the most likely hidden state path k* (return state instead of idx)
+        """
+        path = []
+        ###################################################
+        # Q3.3 Edit here
+        ###################################################
+        return path
---
+++ Hidden Markov Models/tagger.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from util import accuracy
+from hmm import HMM
+
+# TODO:
+def model_training(train_data, tags):
+	"""
+	Train HMM based on training data
+
+	Inputs:
+	- train_data: (1*num_sentence) a list of sentences, each sentence is an object of line class
+	- tags: (1*num_tags) a list of POS tags
+
+	Returns:
+	- model: an object of HMM class initialized with parameters(pi, A, B, obs_dict, state_dict) you calculated based on train_data
+	"""
+	model = None
+	###################################################
+	# Edit here
+	###################################################
+	return model
+
+# TODO:
+def sentence_tagging(test_data, model, tags):
+	"""
+	Inputs:
+	- test_data: (1*num_sentence) a list of sentences, each sentence is an object of line class
+	- model: an object of HMM class
+
+	Returns:
+	- tagging: (num_sentence*num_tagging) a 2D list of output tagging for each sentences on test_data
+	"""
+	tagging = []
+	###################################################
+	# Edit here
+	###################################################
+	return tagging
